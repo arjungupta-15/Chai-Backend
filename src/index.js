49e404b65b69352dc70cb564d7fe42df87b28ca6
Arjun Gupta <140874081+arjungupta-15@users.noreply.github.com>
2024-04-27 21:42:23 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant.js";
+import connectdb from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectdb()
+
+/*
+import express from "express"
+const app = express();
+(async () => {
+    try {
+      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror",(error)=>{
+            console.log("error",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error",error);
+        throw error
+    }
+})()
+*/
